9cd19fadf416c1f43b7b5d1e9cff8a505b753829
Krayseer <krayseer@gmail.com>
2024-12-20 00:30:08 +0500

    Доработки по деплою

--- splash-api/src/main/java/ru/anykeyers/commonsapi/config/EurekaConfig.java
+++ splash-api/src/main/java/ru/anykeyers/commonsapi/config/EurekaConfig.java
@@ -1,8 +1,11 @@
 package ru.anykeyers.commonsapi.config;
 
+import com.netflix.discovery.DefaultEurekaClientConfig;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,10 +19,23 @@ public class EurekaConfig {
     @Autowired(required = false)
     private EurekaInstanceConfigBean eurekaInstanceConfigBean;
 
+    @Autowired(required = false)
+    private EurekaClientConfigBean eurekaClientConfigBean;
+
+    @Value("${DISCOVERY_HOST}")
+    private String discoveryHost;
+
+    @Value("${DISCOVERY_PORT}")
+    private String discoveryPort;
+
     @PostConstruct
     public void configureEurekaInstance() {
         // Переопределение настройки клиентов для использования их через IP адрес, а не hostname
         eurekaInstanceConfigBean.setPreferIpAddress(true);
+
+        // Переопределение зоны нахождения registry-service
+        String zone = String.format("http://%s:%s/eureka", discoveryHost, discoveryPort);
+        eurekaClientConfigBean.getServiceUrl().put(DefaultEurekaClientConfig.DEFAULT_ZONE, zone);
     }
 
 }
