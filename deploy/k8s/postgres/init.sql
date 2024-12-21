ea1f9c2f94878dbbcd0280f76f58179a930da9fe
Krayseer <krayseer@gmail.com>
2024-12-21 21:20:29 +0500

    Реализация файлов для деплоя приложения в Kubernetes

---
+++ deploy/k8s/postgres/init.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE "configurationdb";
+CREATE DATABASE "orderdb";
+CREATE DATABASE "businessorderdb";
+CREATE DATABASE "servicedb";
+CREATE DATABASE "statisticsdb";
+CREATE DATABASE "notificationdb";
+CREATE DATABASE "chatdb";
+
+GRANT ALL PRIVILEGES ON DATABASE "configurationdb" TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE "orderdb" TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE "businessorderdb" TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE "servicedb" TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE "statisticsdb" TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE "notificationdb" TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE "chatdb" TO postgres;
